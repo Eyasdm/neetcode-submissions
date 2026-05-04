f4df2987ce9683d32ed7a0e0c2aa7848d3f5a06c
Eyas Mohammed <eyasadam878@gmail.com>
2026-05-04 09:35:54 +0700

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+       const normalize = (str) => str.split("").sort().join("");
+  let result = [],
+    compare = new Map();
+
+  for (let word of strs) {
+    result.push(normalize(word));
+  }
+  for (let i = 0; i < strs.length; i++) {
+    if (!compare.has(result[i])) compare.set(result[i], []);
+
+    compare.get(result[i]).push(strs[i]);
+  }
+
+  result = [];
+  for (const [key, value] of compare) {
+    result.push(value);
+  }
+  return result;
+    }
+}
