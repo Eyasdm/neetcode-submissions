7bf3509c20de8deefcb489e844c249bc592f172c
Eyas Mohammed <eyasadam878@gmail.com>
2026-05-09 11:10:50 +0700

    Add: move-zeroes - submission-0

---
+++ Data Structures & Algorithms/move-zeroes/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    moveZeroes(nums) {
+        let left = 0
+
+        for (let right=0 ; right < nums.length ; right++) {
+            if(nums[right] !== 0 ){
+               const temp = nums[left]
+                nums[left] = nums[right]
+                nums[right] = temp
+                left++
+            } 
+        }
+        return nums
+    }
+}
