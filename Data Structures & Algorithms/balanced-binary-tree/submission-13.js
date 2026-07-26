bfa51ae48644f89f6c9986667380f37ade95c382
Eyas Mohammed <eyasadam878@gmail.com>
2026-07-26 07:57:55 +0700

    Add: balanced-binary-tree - submission-13

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-13.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isBalanced(root) {
+        if (!root ) return true;
+
+        function depth(node) {
+            if (!node) return 0;
+
+            const left = depth(node.left);
+            const right = depth(node.right);
+
+            console.log(`Node ${node.val}: left=${left}, right=${right}`);
+
+            if (Math.abs(left - right) > 1) return -2;
+
+            return Math.max(left, right) + 1;
+        }
+
+        const left = !root.left ? 0 : depth(root.left);
+        const right = !root.right ? 0 : depth(root.right);
+
+        if(left === -2 || right === -2) return false
+        return !(Math.abs(left - right) > 1);
+    }
+}
