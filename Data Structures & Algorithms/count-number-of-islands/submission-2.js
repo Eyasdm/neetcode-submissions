989865aa3b972dedb83ced13b253d7cc32821d0a
Eyas Mohammed <eyasadam878@gmail.com>
2026-09-24 16:02:50 +0700

    Add: count-number-of-islands - submission-2

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-2.js
@@ -0,0 +1,66 @@
+class Solution {
+    /**
+     * @param {character[][]} grid
+     * @return {number}
+     */
+    numIslands(grid) {
+        if (!grid || grid.length === 0) return 0;
+
+        const ROWS = grid.length;
+        const COLS = grid[0].length;
+        const directions = [
+                    [1, 0],
+                    [-1, 0],
+                    [0, 1],
+                    [0, -1]
+                ];
+
+
+        let islands = 0;
+        let visit = new Set();
+
+        const bfs = (r, c) => {
+            let q = [];
+
+            visit.add(`${r},${c}`);
+            q.push([r, c]);
+
+            while (q.length > 0) {
+                const [row, col] = q.shift();
+
+                
+                for (const [dr, dc] of directions) {
+                    const nr = row + dr;
+                    const nc = col + dc;
+
+                    if (
+                        nr >= 0 &&
+                        nr < ROWS &&
+                        nc >= 0 &&
+                        nc < COLS &&
+                        grid[nr][nc] === "1" &&
+                        !visit.has(`${nr},${nc}`)
+                    ) {
+                        q.push([nr, nc]);
+                        visit.add(`${nr},${nc}`);
+                    }
+                }
+            }
+        };
+
+        for (let r = 0; r < ROWS; r++) {
+            for (let c = 0; c < COLS; c++) {
+
+                if (
+                    grid[r][c] === "1" &&
+                    !visit.has(`${r},${c}`)
+                ) {
+                    bfs(r, c);
+                    islands++;
+                }
+            }
+        }
+
+        return islands;
+    }
+}
